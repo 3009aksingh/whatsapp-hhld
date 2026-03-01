9bf701526a4c5227553a64f3594866ae13daa04a
Ankitkumar Santosh Singh <30768719+3009aksingh@users.noreply.github.com>
2026-03-02 00:08:29 +0530

    changes made , added NEXT_PUBLIC_API_URL in .env of frontend

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
 
   const handleLogin = async () => {
     try {
-      const res = await fetch("http://localhost:5000/login", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
